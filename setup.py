502185064e9e01adb6d2ead3ae1848aeacdd0359
João Faria <joao.faria@astro.up.pt>
2018-07-19 14:22:13 +0100

    add package and keplerian

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='reve',
+      version='0.1',
+      description='Simulate RVs',
+      author='João Faria',
+      author_email='joao.faria@astro.up.pt',
+      license='GPLv3+',
+      url='https://github.com/j-faria/reve',
+      packages=['reve',],
+      intall_requires=['numpy',],
+     )
